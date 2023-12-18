26322b91fde4aa467d4142978088dda908bdcf58
Signol04 <jorgesignol9@gmail.com>
2023-12-17 20:03:40 -0500

    uwu

--- app/src/main/java/pe/com/supertasty/DetallePedidoActivity.kt
+++ app/src/main/java/pe/com/supertasty/DetallePedidoActivity.kt
@@ -9,13 +9,18 @@ import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import pe.com.apptienditarest.util.Utilidad
 import pe.com.supertasty.Adapter.CategoriaComboAdapter
 import pe.com.supertasty.Adapter.ProductoComboAdapter
 import pe.com.supertasty.Entity.CategoriaEntity
 import pe.com.supertasty.Entity.ClienteEntity
+import pe.com.supertasty.Entity.DetallePedidoEntity
+import pe.com.supertasty.Entity.PedidoEntity
 import pe.com.supertasty.Entity.ProductoEntity
 import pe.com.supertasty.Service.CategoriaService
 import pe.com.supertasty.Service.ClienteService
+import pe.com.supertasty.Service.DetallePedidoService
+import pe.com.supertasty.Service.PedidoService
 import pe.com.supertasty.Service.ProductoService
 import pe.com.supertasty.databinding.ActivityDetallepedidoBinding
 import pe.com.supertasty.databinding.ActivityLoginBinding
@@ -23,11 +28,14 @@ import pe.com.supertasty.remote.Api
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.Date
+
 
 class DetallePedidoActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityDetallepedidoBinding
     //creramos un objeto de la entidad
+    private val objutilidad= Utilidad()
     private var objcliente= ClienteEntity()
     //una variable para trabajar con el servicio
     private var clienteService: ClienteService?=null
@@ -38,6 +46,8 @@ class DetallePedidoActivity : AppCompatActivity() {
     var nom=""
 
 
+    private var detallepedidoService: DetallePedidoService?=null
+    private var pedidoService: PedidoService? =null
     private var productoService: ProductoService? = null
     private var categoriaService: CategoriaService? = null
     private var registroproductos: List<ProductoEntity>? = null
@@ -57,47 +67,123 @@ class DetallePedidoActivity : AppCompatActivity() {
 
         val context = getApplicationContext()
         objcliente = ClienteEntity()
-        registrocliente=ArrayList()
-        registroproductoxcategoria= ArrayList()
+        registrocliente = ArrayList()
+        registroproductoxcategoria = ArrayList()
         registroproductos = ArrayList()
         registrocategoria = ArrayList()
+        detallepedidoService = Api.detallePedidoService
         categoriaService = Api.categoriaService
         productoService = Api.productoService
-        clienteService= Api.clienteService
-
+        clienteService = Api.clienteService
+        pedidoService = Api.pedidoService
 
 
         val idcliente = intent.getStringExtra("idCliente").toString().toLong()
-        Toast.makeText(this,"CLIENTE ACCEDIO: "+idcliente.toString(), Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "CLIENTE ACCEDIO: " + idcliente.toString(), Toast.LENGTH_LONG).show()
         MostrarCliente(this, idcliente)
 
         cargarComboCategoria(context)
 
         var ic = cargarComboProducto(context)
-        registrosProducto(context,ic.toLong())
+        registrosProducto(context, ic.toLong())
 
 
 
         binding.cboProducto.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
                 pro = pdenCombo.get(position).codigo
-                if(pro > 0){
+                if (pro > 0) {
                     var precio = pdenCombo.get(position).precio
                     binding.txtIdproducto.text = pro.toString()
                     binding.txtPrePed.text = precio.toString()
-                }
-                else{
+                } else {
                     binding.txtIdproducto.text = ""
                     binding.txtPrePed.text = ""
                 }
 
             }
+
             override fun onNothingSelected(parent: AdapterView<*>) {
 
 
             }
         }
+        binding.btnRegistrarPed.setOnClickListener {
+            // Recoge los datos de los campos de texto
+            val idCategoria = binding.txtIdCategoria.text.toString().toLong()
+            val idProducto = binding.txtIdproducto.text.toString().toLong()
+            val cantidad = binding.txtCantPed.text.toString().toLong()
+            val precio = binding.txtPrePed.text.toString().toDouble()
+
+            val detallePedido = DetallePedidoEntity(
+                codigo = 0,
+                precio = precio,
+                cantidad = cantidad,
+                categoria = CategoriaEntity(),
+                producto = ProductoEntity()
+            )
+            val pedido = PedidoEntity(
+                codigo = 0,
+                estado = true,
+                fechaRegistro = Date(),
+                total = precio * cantidad,
+                codCliente = idcliente
+            )
+
+            registrarPedido(this, pedido)
+            registrarDetallePedido(this, detallePedido)
+        }
+
+
     }
+    fun registrarPedido(context: Context, pedido: PedidoEntity) {
+        val pedidoService = Api.pedidoService
+        val call = pedidoService?.addPedido(pedido)
+        call?.enqueue(object : Callback<PedidoEntity?> {
+            override fun onResponse(call: Call<PedidoEntity?>, response: Response<PedidoEntity?>) {
+                if (response.isSuccessful) {
+                    Log.d("RegistroPedido", "Pedido registrado correctamente")
+                } else {
+                    Log.e("RegistroPedido", "Error al registrar el pedido")
+                }
+            }
+
+            override fun onFailure(call: Call<PedidoEntity?>, t: Throwable) {
+                Log.e("RegistroPedido", "Error en la conexión: ${t.message}")
+            }
+        })
+    }
+
+    fun registrarDetallePedido(context: Context, detallePedido: DetallePedidoEntity) {
+        val detallePedidoService = Api.detallePedidoService
+
+        val call = detallePedidoService?.add(detallePedido)
+        call?.enqueue(object : Callback<DetallePedidoEntity?> {
+            override fun onResponse(
+                call: Call<DetallePedidoEntity?>,
+                response: Response<DetallePedidoEntity?>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d("RegistroDetallePedido", "Detalle del pedido registrado correctamente")
+                } else {
+                    Log.e("RegistroDetallePedido", "Error al registrar el detalle del pedido")
+                }
+            }
+
+            override fun onFailure(call: Call<DetallePedidoEntity?>, t: Throwable) {
+                Log.e("RegistroDetallePedido", "Error en la conexión: ${t.message}")
+            }
+        })
+    }
+
+
+
+
 
     private fun cargarComboCategoria(context:Context) {
         val call = categoriaService!!.findAll()
@@ -127,7 +213,7 @@ class DetallePedidoActivity : AppCompatActivity() {
     }
 
 
-    fun registrosProducto(context: Context,idcat:Long){
+        fun registrosProducto(context: Context,idcat:Long){
         val call=productoService!!.findAll()
         call!!.enqueue(object : Callback<List<ProductoEntity>?> {
             override fun onResponse(
